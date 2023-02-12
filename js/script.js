305cbe03e94a08f2f5dd280659cc2ab1f3c8bcfa
DilmurodjonT <dilmurod_tadjibaev@mail.ru>
2023-02-12 21:54:07 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const showBtn = document.getElementById("show-btn");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("close-btn");
+const overlay = document.getElementById("overlay");
+
+// add classList hidden
+const addHidden = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// remove classList hidden
+const removeHidden = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+showBtn.addEventListener("click", removeHidden);
+
+// tepada ihchamlashtirib function ga soldik
+// showBtn.addEventListener("click", () => {
+//   modal.classList.remove("hidden");
+//   overlay.classList.remove("hidden");
+// });
+
+closeBtn.addEventListener("click", addHidden);
+
+overlay.addEventListener("click", addHidden);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape") {
+    addHidden();
+  }
+});
+
+// tepada ihchamlashtirdik buni
+// closeBtn.addEventListener("click", () => {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+
+// overlay.addEventListener("click", () => {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+
+// document.addEventListener("keydown", (e) => {
+//   if (e.key == "Escape") {
+//     modal.classList.add("hidden");
+//     overlay.classList.add("hidden");
+//   }
+// });
